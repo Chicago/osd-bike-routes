2f50f7a7100734a5b3cc8f6de7f09d74c081e79f
tomschenkjr <tomschenkjr@gmail.com>
2013-09-04 14:07:56 -0500

    Fixes #3

--- examples/Importing GeoJSON Python Demo.py
+++ examples/Importing GeoJSON Python Demo.py
@@ -1,11 +1,11 @@
 # TITLE: Importing GeoJSON Example in Python
 # AUTHOR: Tom Schenk Jr., City of Chicago
 # CREATED: 2013-01-23
-# UPDATED: 2013-02-03
+# UPDATED: 2013-09-04
 # NOTES: Quick example to import GeoJSON data in Python.
 # MODULES: json
 
 import json
 bikeroutes_json = open('PATH/TO/FILE/data/Bikeroutes.json', 'r')
-bike_routes = json.load(json_data)
-json_data.close()
+bike_routes = json.load(bikeroutes_json)
+json_data.close(bikeroutes_json)
